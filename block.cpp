8280c51dc482a67db1a1523814fd29ff3177bb9b
adpascual <73863640+adpascual@users.noreply.github.com>
2021-11-09 11:57:48 -0800

    Update block.cpp

--- block.cpp
+++ block.cpp
@@ -1,16 +1,17 @@
-#include<iostream>
+//myprog01.cpp
+//Author: Andrew Pascual
+#include <iostream>
 using namespace std;
-int main(){
-int rows,columns,i,j;
-do{
-cout << "Enter number of rows and columns:"<<endl;
-cin >> rows >> columns;
-for(int i=0;i<rows;i++){
-for(int j=0;j<columns;j++){
-cout << "X.";
-}
-cout << endl;
-}
-}while(rows!=0||columns!=0);
-return 0;
+ 
+int main() {
+    // Simple for loop that counts down from n to 1
+ 
+    int n=5;
+ 
+    for (int i=n; i>0; i--) {
+       cout << i << " ";
+       cout << endl;
+    }
+ 
+    return 0;
 }
